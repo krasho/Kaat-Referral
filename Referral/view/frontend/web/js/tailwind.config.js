64f4ab810326ccb3872c49d30ae532fc80e31d69
Jose Galicia <crasho1481@gmail.com>
2024-01-20 20:05:45 -0500

    add tailwind css

---
+++ Referral/view/frontend/web/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      'app/code/Kaat/Referral/../**/*.phtml',
+      'app/code/Kaat/Referral/../**/*.html',
+      'app/code/Kaat/Referral/../**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
